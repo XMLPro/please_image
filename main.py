5c43acd8ffbd20b438bd3e2e779894db0347d56c
ctare <sankaku900622@yahoo.co.jp>
2017-10-28 21:33:39 +0900

    httpを保存する

--- main.py
+++ main.py
@@ -14,42 +14,42 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(os.environ.get('TOKEN'))
-handler = WebhookHandler(os.environ.get('SECRET'))
+# line_bot_api = LineBotApi(os.environ.get('TOKEN'), "")
+# handler = WebhookHandler(os.environ.get('SECRET'), "")
 
 @app.route("/", methods=['GET'])
 def hello():
     return "hello"
-
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    request = event.message.text
-    if request.startswith("plz"): #特定の文字列から始まるなら
-        result = request.split(" ")[1]
-        url = search.one(result)
-        image_message = ImageSendMessage(
-            original_content_url=url,
-            preview_image_url=url
-        )
-        line_bot_api.reply_message(event.reply_token,image_message)
-
-
+#
+# @app.route("/callback", methods=['POST'])
+# def callback():
+#     # get X-Line-Signature header value
+#     signature = request.headers['X-Line-Signature']
+#
+#     # get request body as text
+#     body = request.get_data(as_text=True)
+#     app.logger.info("Request body: " + body)
+#
+#     # handle webhook body
+#     try:
+#         handler.handle(body, signature)
+#     except InvalidSignatureError:
+#         abort(400)
+#
+#     return 'OK'
+#
+# @handler.add(MessageEvent, message=TextMessage)
+# def handle_message(event):
+#     request = event.message.text
+#     if request.startswith("plz"): #特定の文字列から始まるなら
+#         result = request.split(" ")[1]
+#         url = search.one(result)
+#         image_message = ImageSendMessage(
+#             original_content_url=url,
+#             preview_image_url=url
+#         )
+#         line_bot_api.reply_message(event.reply_token,image_message)
+#
+#
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=int(os.environ.get("PORT",5000)))
--- search.py
+++ search.py
@@ -1,8 +1,11 @@
 import os
 import googleapiclient.discovery
 import random
+import requests
 api_key = os.environ.get("GOOGLE_SEARCH_API_KEY")
 search_engine = os.environ.get("GOOGLE_SEARCH_ENGINE")
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
 
 service = googleapiclient.discovery.build(
         "customsearch", "v1", developerKey=api_key)
@@ -22,8 +25,7 @@ def image_search(query):
     return [c["src"]
             for cse_image in cse_images
             if cse_image
-            for c in cse_image
-            if c["src"].startswith("https")]
+            for c in cse_image]
 
 
 def only_https(url_list):
@@ -33,3 +35,15 @@ def only_https(url_list):
 def one(query):
     url_list = image_search(query)
     return random.choice(only_https(url_list))
+
+
+# get root like this: request.url_root
+def one_include_http(query, root):
+    url = random.choice(image_search(query))
+    image = requests.get(url)
+    save_name = "/static/image/TMP_IMAGE." + url.split(".")[-1]
+    save_path = base_dir + save_name
+    if image.status_code == 200:
+        with open(save_path, "wb") as f:
+            f.write(image.content)
+    return root + save_name
