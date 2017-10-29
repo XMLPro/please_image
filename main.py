85800621fb05cb2b74ee78e71d83c4f3395ad51e
projectormato <k.keita811.anuy@gmail.com>
2017-10-29 16:49:21 +0900

    revert

--- main.py
+++ main.py
@@ -50,8 +50,8 @@ def handle_message(event):
         )
         line_bot_api.reply_message(event.reply_token,image_message)
         #by debug
-        mage_message = TextSendMessage(text=url)
-        line_bot_api.reply_message(event.reply_token,mage_message)
+        # mage_message = TextSendMessage(text=url)
+        # line_bot_api.reply_message(event.reply_token,mage_message)
 
     # そうじゃないならとりあえず何もしない
 
